05f68fd4051566dcac15b9cd2e4244ed84f901f0
Hector Garcia <garciaph@148.61.186.12>
2020-01-31 16:54:18 -0500

    Lab3_Part3_Redo

---
+++ Lab3_Part3_Redo/main.c
@@ -0,0 +1,18 @@
+#include "msp.h"
+#include <stdint.h>
+
+int main(void) {
+    volatile uint32_t i;
+    uint32_t Value = 10000;
+
+    WDT_A->CTL = WDT_A_CTL_PW |             // Stop WDT
+                 WDT_A_CTL_HOLD;
+
+    P1->DIR |= BIT0;                        // P1.0 set as output
+
+    while (1)                               // continuous loop
+    {
+        P1->OUT ^= BIT0;                    // Blink P1.0 LED
+        for (i = Value; i > 0; i--);        // Delay
+    }
+}
