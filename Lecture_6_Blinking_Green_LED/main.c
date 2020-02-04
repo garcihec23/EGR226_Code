ce3bd522deb7ddf161051be857e0050a846b25b8
Hector Garcia <garciaph@148.61.177.173>
2020-02-03 22:26:29 -0500

    Toggling the LEDs not working with XOR gate. Look into it later

--- Lecture_6_Blinking_Green_LED/main.c
+++ Lecture_6_Blinking_Green_LED/main.c
@@ -7,10 +7,25 @@ void main(void)
     P2SEL0 &= 0;    // SEL 0 and SEL 1 are 0 therefore configured as Simple I/O
     P2SEL1 &= 0;
 
-    P2DIR |= 0x07;      // Setting pins 1, 2, and 3 as outputs
+    P2DIR |= 7;      // Setting pins 1, 2, and 3 as outputs
 
 
     while (1) {
+
+        P2OUT ^= 7;
+        delay(1000);
+        /*
+        P2OUT ^= BIT1;
+        delay(1000);
+
+        P2OUT ^= BIT2;
+        delay(1000);
+
+        P2OUT ^= 0x07;
+        delay(1000);
+        */
+
+        /*
         P2OUT |= BIT0;      // Sets pin 1 in port 2 HIGH (Turns on the Red LED)
         delay(1000);
 
@@ -34,6 +49,7 @@ void main(void)
 
         P2OUT &= ~0x07;      // Sets pin 1 in port 2 HIGH (Turns off the RGB LED)
         delay(1000);
+        */
 
     }
 }
