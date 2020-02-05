2575e03a2bdb89bd101eecbc281e9741bae2a692
Hector Garcia <garciaph@148.61.177.173>
2020-02-04 23:04:20 -0500

    Update

--- Lab_4_Digital_IOs/main.c
+++ Lab_4_Digital_IOs/main.c
@@ -17,25 +17,25 @@ void main(void)
 {
 	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watch dog timer
 
-	P1SEL0 &= ~0x02;        // Setting SEL0 and SEL1 in Port 1 to be Simple I/O
-	P1SEL1 &= ~0x02;
+	P1->SEL0 &= ~0x02;        // Setting SEL0 and SEL1 in Port 1 to be Simple I/O
+	P1->SEL1 &= ~0x02;
 
-    P1DIR &= ~0x02;         // Setting the direction of Port 1 Pin 1 to be an input
-    P1REN |= 0x02;          // Internal Resistor is enabled
-    P1OUT |= 0x02;          // Resistor is configured as a Pull-Up Resistor
-                            // We want a pull-up resistor because we are reading for
-                            // 0's to verify the button was pressed
+    P1->DIR &= ~0x02;         // Setting the direction of Port 1 Pin 1 to be an input
+    P1->REN |= 0x02;          // Internal Resistor is enabled
+    P1->OUT |= 0x02;          // Resistor is configured as a Pull-Up Resistor
+                              // We want a pull-up resistor because we are reading for
+                              // 0's to verify the button was pressed
 
-	P2SEL0 &= ~0x07;        // Setting SEL0 and SEL1 in Port 2 to be Simple I/O
-	P2SEL1 &= ~0x07;
+	P2->SEL0 &= ~0x07;        // Setting SEL0 and SEL1 in Port 2 to be Simple I/O
+	P2->SEL1 &= ~0x07;
 
-	P2DIR |= 0x07;          // Sets pins 1, 2, 3 in Port 2 as outputs (RGB LED)
+	P2->DIR |= 0x07;          // Sets pins 1, 2, 3 in Port 2 as outputs (RGB LED)
 
 	if (SwitchBounce()) {
-	    P2OUT |= 0x07;      // Turns on RGB LED
+	    P2->OUT |= 0x07;      // Turns on RGB LED
 	}
 	else {
-	    P2OUT &= 0x07;      // Turns off RGB LED
+	    P2->OUT &= 0x07;      // Turns off RGB LED
 	}
 
 }
@@ -52,13 +52,15 @@ int SwitchBounce() {
     // we are delaying 5ms to verify the button was actually pressed
     // if it is pressed, the flag is set HIGH (1)
 
-    if ((P1IN & 0x02)) {
-        _delay_cycles(15000);      // When cycles equals 15,000, the delay will be 5ms
+    while (1) {
+        if ((P1->IN & 0x02)) {
+            _delay_cycles(15000);      // When cycles equals 15,000, the delay will be 5ms
 
-        if ((P1IN & 0x02)) {
-            flag = 1;       // flag is set to 1 meaning button is pressed
+            if ((P1->IN & 0x02)) {
+                flag = 1;       // flag is set to 1 meaning button is pressed
+            }
         }
-    }
+        return flag;
+}
 
-    return flag;
 }
